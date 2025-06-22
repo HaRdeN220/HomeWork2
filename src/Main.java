9d8bbf2843376862cad3ade5bbe78ed2e0efd6c6
egor <vlad.filin_2000@mail.ru>
2025-06-22 16:42:10 +0300

    6 задание

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-   short paint = 120;
-   short whiteColour = 2;
-   short brownColour = 4;
-   System.out.println("В школе, где " + (paint/(whiteColour + brownColour)) + " классов, нужно " +
-           ((paint/(whiteColour + brownColour))*whiteColour)+" банок белой краски и "+ ((paint/(whiteColour + brownColour))*brownColour) +
-           " банок коричневой краски.");
+int bananas = 5 * 80;
+int milk = (200 / 100) * 105;
+int iceCream = 2 * 100;
+int eggs = 4 * 70;
+float breakFast = (bananas + milk + iceCream + eggs) / 1000f;
+    System.out.println("Общий вес завтрака = " + breakFast);
     }
 }
