f5c504f368a05fa2630dd31873cfdc87fa145eb5
egor <vlad.filin_2000@mail.ru>
2025-06-22 16:34:15 +0300

    4 задание

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-   short time = 2;
-   short bottle = 16;
-   int perfomance = bottle / time;
-        System.out.println("За 20 минут машина произвела " + (perfomance*20) + " штук бутылок");
-        System.out.println("За сутки машина произвела " + (perfomance*60*24) + " штук бутылок");
-        System.out.println("За 3 дня машина произвела " + (perfomance*60*24*3) + " штук бутылок");
-        System.out.println("За месяц машина произвела " + (perfomance*60*24*30) + " штук бутылок");
+   short paint = 120;
+   short whiteColour = 2;
+   short brownColour = 4;
+   System.out.println("В школе, где " + (paint/(whiteColour + brownColour)) + " классов, нужно " +
+           ((paint/(whiteColour + brownColour))*whiteColour)+" банок белой краски и "+ ((paint/(whiteColour + brownColour))*brownColour) +
+           " банок коричневой краски");
     }
 }
