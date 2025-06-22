3c9a49295cb1f173115c94fa8e7fee8472b3983d
egor <vlad.filin_2000@mail.ru>
2025-06-22 16:27:05 +0300

    4 задание

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-   byte LudmilaStudents = 23;
-   byte AnnaStudents = 27;
-   byte EkaterinaStudents = 30;
-   short paper = 480;
-   System.out.println("На каждого ученика рассчитано " + (paper/(LudmilaStudents+AnnaStudents+EkaterinaStudents)) + " листов бумаги");
+   short time = 2;
+   short bottle = 16;
+   int perfomance = bottle / time;
+        System.out.println("За 20 минут машина произвела " + (perfomance*20) + " штук бутылок");
+        System.out.println("За сутки машина произвела " + (perfomance*60*24) + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + (perfomance*60*24*3) + " штук бутылок");
+        System.out.println("За месяц машина произвела " + (perfomance*60*24*30) + " штук бутылок");
     }
 }
